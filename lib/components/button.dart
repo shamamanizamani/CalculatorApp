e57a142f323b9bdcd1480f2996aaa72c192607a6
shamamanizamani <nizamanishamama@gmail.com>
2025-09-28 17:50:49 +0500

    redesign calculator buttons UI for better look

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -3,24 +3,41 @@ import 'package:flutter/material.dart';
 class Button extends StatelessWidget {
   final String Label;
   final Color Colour;
+  final Color textColour;
   final VoidCallback Press;
-  const Button({super.key, required this.Label, this.Colour = Colors.grey, required this.Press});
+  const Button({
+    super.key,
+    required this.Label,
+    this.Colour = const Color(0xFF141414),
+    required this.Press,
+    this.textColour = const Color(0xFFd8d8d8),
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: InkWell(
+        borderRadius: BorderRadius.circular(40),
         onTap: Press,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 7, vertical:10),
+          padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 10),
           child: Container(
             decoration: BoxDecoration(
               color: Colour,
-              shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(40),
             ),
             height: 75,
             width: 75,
-            child: Center(child: Text(Label, style: TextStyle(color: Colors.white))),
+            child: Center(
+              child: Text(
+                Label,
+                style: TextStyle(
+                  color: textColour,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
           ),
         ),
       ),
